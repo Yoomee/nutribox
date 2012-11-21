ea31fa80a982661413cdf0072b0342362ac2d0ed
Matt Atkins <matt@yoomee.com>
2012-11-21 11:54:19 +0000

    Add redirect remove www subdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery
 
+  before_filter :redirect_if_www
   before_filter :authenticate
   
   AUTH_USERS = { "nutribox" => "nutribox123" }
@@ -12,6 +13,12 @@ class ApplicationController < ActionController::Base
     return true unless Rails.env.production?
     authenticate_or_request_with_http_basic do |username|
       AUTH_USERS[username]
+    end
+  end
+  
+  def redirect_if_www
+    if request.subdomain.present? && request.subdomain == "www"
+      redirect_to request.url.sub("www.",'')
     end
   end
 
