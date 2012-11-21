bf414f3e09009a7687741aedeee0f20d55e4e21e
Matt Atkins <matt@yoomee.com>
2012-11-21 10:30:06 +0000

    Blog page summary, tidy up page#edit, update ym_cms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,6 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate
   
   AUTH_USERS = { "nutribox" => "nutribox123" }
-  
-  def after_sign_in_path_for_resource(user)
-    orders_path
-  end
 
   private
   def authenticate
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   skip_before_filter :authenticate
   
   def index
+    @latest_blog_post = Page.find_by_slug("blog").children.latest.first
     render :action => "logged_out_index" unless current_user || Rails.env.development?
   end
 
