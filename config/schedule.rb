f1b1d62e168f9769f49003913d306996741075a7
Matt Atkins <matt@yoomee.com>
2012-12-13 14:14:32 +0000

    Add cron task to sync with Xero every hour

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,3 +4,7 @@ every 1.day, :at => '6pm' do
   set :output, File.expand_path("#{File.dirname(__FILE__)}/../../../shared/sphinx_rebuilds.log")
   rake "ts:index -t"
 end
+
+every 1.hour do
+  rake "nutribox:xero"
+end
