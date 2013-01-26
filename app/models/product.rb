a8866046adb27d0744d5ebc5692233443f9f5e15
Andy Mayer <andy@yoomee.com>
2013-01-26 19:17:02 +0000

    Add validation to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,9 @@ class Product < ActiveRecord::Base
   include CanCan::Ability
   image_accessor :image    
   
+  validates :name, :description, :month, :year, :box_type, :image, :presence => true
+  
+  
   def self.months
     months = []
     (1..12).each {|m| months << [Date::MONTHNAMES[m], m]}
