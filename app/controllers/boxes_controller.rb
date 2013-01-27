854f4fc80645fb782006edd403694c3ff8e6fcd2
Andy Mayer <andy@yoomee.com>
2013-01-27 16:32:14 +0000

    Pagination and other improvements

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -2,20 +2,11 @@ class BoxesController < ApplicationController
   load_and_authorize_resource
 
   def show
-    @type = params[:type]
-    @year = params[:year].to_i
-    @month = params[:month].to_i
-    if !Box.types.include?(@type) || @type == 'both'
+    @box = Box.new(params[:year], params[:month], params[:type])
+    if !Box.types.include?(@box.type) || @box.type == 'both'
       not_found
     else
-      @box = Box.new(@year, @month, @type)
-      if @month == 0
-          redirect_to box_path(:year => @year-1, :month => 12, :type => @type)
-      elsif @month == 13
-        redirect_to box_path(:year => @year+1, :month => 1, :type => @type)
-      else
-        @products = Product.where("month = ? AND year= ? AND (box_type = ? OR box_type ='both')", @month, @year, @type).order(:name)
-      end
+      @products = Product.where("month = ? AND year= ? AND (box_type = ? OR box_type ='both')", @box.month, @box.year, @box.type).order(:name)
     end
     
   end
--- app/models/box.rb
+++ app/models/box.rb
@@ -1,21 +1,49 @@
 class Box
   
   include CanCan::Ability
+  attr_accessor :month, :year, :type
 
-  def initialize(month, year, type)
-    @month = month
-    @year = year
+  def initialize(year, month, type)
+    @month = month.to_i
+    @year = year.to_i
     @type = type
   end
   
-  def in_the_future?
-    false
+  def previous_month
+    ("1/#{@month}/#{@year}".to_datetime - 1.month).month
   end
   
+  def next_month
+    ("1/#{@month}/#{@year}".to_datetime + 1.month).month
+  end
+  
+  def previous_year
+    ("1/#{@month}/#{@year}".to_datetime - 1.month).year
+  end
+  
+  def next_year
+    ("1/#{@month}/#{@year}".to_datetime + 1.month).year
+  end
+  
+  def in_the_future?
+    return false if @year < Time.now.year
+    return true if @month > Time.now.month
+    return false if Time.now.day > SHIPPING_DAY
+    true
+  end
+    
+  def last_box?
+    Product.where("month = ? AND year= ? AND (box_type = ? OR box_type ='both')", next_month, next_year, @type).empty?
+  end  
+  
+  def first_box?
+    Product.where("month = ? AND year= ? AND (box_type = ? OR box_type ='both')", previous_month, previous_year, @btype).empty?
+  end
+    
   def self.types
     ["mini", "standard", "both"]
   end
   
-  
+SHIPPING_DAY = 11
   
 end
