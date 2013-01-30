68edf1885ee2bb9dc649415cca0b02b18380c987
Andy Mayer <andy@yoomee.com>
2013-01-30 10:25:09 +0000

    Allow comments to be faked by admins as well as ratings

--- app/controllers/survey_answers_controller.rb
+++ app/controllers/survey_answers_controller.rb
@@ -1,9 +1,7 @@
 class SurveyAnswersController < ApplicationController
   
  def rate   
-   if params[:survey_id].to_i.zero?
-     @rating = SurveyAnswer.new(:survey => Survey.new(), :product_id => params[:product_id], :rating => params[:rating])   
-   else
+   if !(@rating = fake_rating(params))
      @rating = SurveyAnswer.find_or_initialize_by_survey_id_and_product_id(params[:survey_id],params[:product_id])
      @rating.update_attribute(:rating, params[:rating])
    end
@@ -17,14 +15,28 @@ class SurveyAnswersController < ApplicationController
  end
  
  def update
-   @rating = SurveyAnswer.find_or_initialize_by_survey_id_and_product_id(params[:survey_answer][:survey_id],params[:survey_answer][:product_id])
-   @rating.comment = params[:survey_answer][:comment]
-   @rating.save
+   if !(@rating = fake_rating(params[:survey_answer]))
+     @rating = SurveyAnswer.find_or_initialize_by_survey_id_and_product_id(params[:survey_answer][:survey_id],params[:survey_answer][:product_id])
+     @rating.comment = params[:survey_answer][:comment]
+     @rating.save
+   end 
+   puts "----" 
+   puts @rating.inspect  
  end
 
  def create
    update
    render "update"
+ end
+ 
+ private
+ 
+ def fake_rating(params)
+   if params[:survey_id].to_i.zero?
+     @rating = SurveyAnswer.new(:survey => Survey.new(), :product_id => params[:product_id], :rating => params[:rating])
+   else 
+     false
+   end      
  end
  
   
