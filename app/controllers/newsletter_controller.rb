f387960d291b74183be752b5855798ca78bbcdf8
Matt Atkins <matt@yoomee.com>
2013-02-15 12:15:22 +0000

    Explicit url redirect

--- app/controllers/newsletter_controller.rb
+++ app/controllers/newsletter_controller.rb
@@ -3,7 +3,7 @@ class NewsletterController < ApplicationController
   
 	def subscribe
     if request.method.to_s == "POST"
-      redirect_to subscribe_newsletter_path(:email => params[:email]), :protocol => "http://"
+      redirect_to "http://thenutribox.com/newsletter/subscribe?email=#{params[:email]}"
     else
       uri = URI.parse("http://thenutribox.us6.list-manage1.com/subscribe/post")
       response = Net::HTTP.post_form(uri, {:u => "44bdbc0e5b3036d3b03028268", :id => "f573663757", :EMAIL => params[:email]})
