63d53e5a5bc9ce1bff4971b30475e84bafff8d12
Edward Andrews <edward@aldreth.com>
2013-07-09 16:00:46 +0100

    Get correct week

--- app/models/order.rb
+++ app/models/order.rb
@@ -41,10 +41,11 @@ class Order < ActiveRecord::Base
   class << self
     
     def repeatable_by_week(date)
-      if date.shipping_week == 4 && !date.five_fridays_in_month?
+      date_for_correct_week = date - 1.day
+      if date_for_correct_week.shipping_week == 4 && !date_for_correct_week.five_fridays_in_month?
         repeatable.where(:shipping_week => [4, 5])
       else
-        repeatable.where(:shipping_week => date.shipping_week)
+        repeatable.where(:shipping_week => date_for_correct_week.shipping_week)
       end
     end
 
