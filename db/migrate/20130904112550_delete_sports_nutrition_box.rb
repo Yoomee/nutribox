ba170ebfc82bebec56a0eb2083fad4dd9fce0c75
Edward Andrews-Hodgson <edward@yoomee.com>
2013-09-04 15:20:54 +0100

    Set old sports box orders to new sports box

--- db/migrate/20130904112550_delete_sports_nutrition_box.rb
+++ db/migrate/20130904112550_delete_sports_nutrition_box.rb
@@ -1,6 +1,9 @@
 class DeleteSportsNutritionBox < ActiveRecord::Migration
   def up
-  	AvailableOrderOption.find_by_name('Sports Nutrition').destroy
+    old_sports_box = AvailableOrderOption.find_by_name('Sports Nutrition')
+    new_sports_box = AvailableOrderOption.find_by_name('The sports performance')
+    Order.where(:theme_id => old_sports_box.id).update_all(:theme_id => new_sports_box.id)
+    old_sports_box.destroy
   end
 
   def down
