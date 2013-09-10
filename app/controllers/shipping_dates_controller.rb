f142acc974465c357c3d3350e4345f666af6028e
Matt Atkins <matt@yoomee.com>
2013-09-10 12:12:40 +0100

    Split spreadsheet downloads by option

--- app/controllers/shipping_dates_controller.rb
+++ app/controllers/shipping_dates_controller.rb
@@ -2,13 +2,21 @@ class ShippingDatesController < ApplicationController
   def show
     @shipping_date = ShippingDate.find_by_param(params[:id])
     authorize! :show, @shipping_date
+    order_options = [[nil, "classic"]] + AvailableOrderOption.order("created_at DESC").map{|aoo| [aoo.id, aoo.option]}
     respond_to do |format|
       format.html do
-        @deliveries = [:mini,:standard].map{|box_type| [box_type,@shipping_date.deliveries.where(:box_type => box_type)]}
+        @deliveries = [].tap do |deliveries|
+          [:mini,:standard].each do |box_type|
+            order_options.each do |option|
+              deliveries << [box_type, option, @shipping_date.deliveries.where(:box_type => box_type, :option_id => option.first)]
+            end
+          end
+        end
       end
       format.xls do
-        headers["Content-Disposition"] = "attachment; filename=\"#{params[:id]}-#{params[:box]}.xls\"" 
-        @deliveries = @shipping_date.deliveries.where(:box_type => params[:box])
+        order_option = order_options.select{|option| option[0].to_s == params[:option_id].to_s}.first
+        headers["Content-Disposition"] = "attachment; filename=\"#{params[:id]}-#{params[:box]}-#{order_option.last.parameterize}.xls\"" 
+        @deliveries = @shipping_date.deliveries.where(:box_type => params[:box], :option_id => order_option.first)
         @shipping_date.update_downloaded(params[:box])
       end
     end
