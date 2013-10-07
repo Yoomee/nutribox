8dbb7c90a558ebd6feddfb18aabf1a446b315c54
Edward Andrews-Hodgson <edward@yoomee.com>
2013-10-07 12:23:10 +0100

    Make referrals work with new order process

--- app/controllers/referrals_controller.rb
+++ app/controllers/referrals_controller.rb
@@ -6,7 +6,19 @@ class ReferralsController < ApplicationController
     if User.exists?(:referral_code => code) && !current_user
       session[:referral_code] = code
     end
-    @order = Order.new
+        gift = params[:gift].present?
+    @order = Order.new(:gift => gift)
+    @themes = AvailableOrderOption.order(:position)
+    if gift
+      @custom_page_title = YmSnippets::Snippet.find_by_slug('meta_gift_page_title').text
+      @custom_page_description = YmSnippets::Snippet.find_by_slug('meta_gift_page_description')
+      @custom_page_keywords = YmSnippets::Snippet.find_by_slug('meta_gift_page_keywords')
+    else
+      @order.discount_code = DiscountCode.default
+      @custom_page_title = YmSnippets::Snippet.find_by_slug('meta_join_page_title')
+      @custom_page_description = YmSnippets::Snippet.find_by_slug('meta_join_page_description')
+      @custom_page_keywords = YmSnippets::Snippet.find_by_slug('meta_join_page_keywords')
+    end
     render :template => "orders/new"
   end
   
