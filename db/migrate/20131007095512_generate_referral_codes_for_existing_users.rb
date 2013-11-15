2f0015498406ab369f5ee5acbf3b0597869d40d7
Matt Atkins <matt@yoomee.com>
2013-11-15 09:33:39 +0000

    Rename referral code migration

--- db/migrate/20131007095512_generate_referral_codes_for_existing_users.rb
+++ db/migrate/20131007095512_generate_referral_codes_for_existing_users.rb
@@ -1,4 +1,4 @@
-class CreateReferralsForExistingUsers < ActiveRecord::Migration
+class GenerateReferralCodesForExistingUsers < ActiveRecord::Migration
   def up
     User.all.each do |user|
       user.send(:generate_referral_code)
